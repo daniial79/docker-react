83a23ea03f16c0f8c0810bfec2da4efe02f8ebdc
daniial79 <dpezeshpoor@yahoo.com>
2023-02-14 11:16:49 +0330

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:16-alpine AS builder
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build 
+
+#run phase
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello From React Server!!! You Mother Fucker (with some changes);)
         </p>
         <a
           className="App-link"
